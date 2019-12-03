9d73f5a6135ee2dd8889591e71d8bd6f396a621e
Morten Turn Pedersen <motp@nodes.dk>
2019-12-03 14:35:00 +0100

    Made concat work as expected

--- src/components/ScriptHelper/ConcatScript.tsx
+++ src/components/ScriptHelper/ConcatScript.tsx
@@ -10,8 +10,7 @@ interface ConcatScriptProps {
 }
 
 class ConcatScriptState {
-    firstHalf: string;
-    secondHalf: string;
+    values: string[];
 }
 
 export class ConcatScript extends Component<ConcatScriptProps, ConcatScriptState> {
@@ -19,37 +18,68 @@ export class ConcatScript extends Component<ConcatScriptProps, ConcatScriptState
         super(props);
 
         let state = new ConcatScriptState();
-        state.firstHalf = "";
-        state.secondHalf = "";
+        state.values = ["", ""];
 
         this.state = state;
+
+        this.addPart = this.addPart.bind(this);
+        this.onChange = this.onChange.bind(this);
+        this.removePart = this.removePart.bind(this);
     }
 
-    onChange(value: string, isFirstHalf: boolean) {
-        let firstHalf: string = this.state.firstHalf;
-        let secondHalf: string = this.state.secondHalf;
+    addPart(): void {
+        let values = this.state.values;
 
-        if(isFirstHalf === true) {
-            firstHalf = value;
-            this.setState({
-                firstHalf: value
-            });
-        } else {
-            secondHalf = value;
-            this.setState({
-                secondHalf: value
-            });
-        }
+        values.push("");
 
-        this.props.onChange("concat(" + firstHalf + "," + secondHalf + ")");
+        this.setState({
+            values: values
+        });
+
+        this.props.onChange(this.getRenderedString(values));
+    }
+
+    getRenderedString(values: string[]): string {
+        return "concat(" + values.join(",") + ")";
+    }
+
+    onChange(value: string, index: number): void {
+        let values = this.state.values;
+
+        values[index] = value;
+
+        this.setState({
+            values: values
+        });
+
+        this.props.onChange(this.getRenderedString(values));
+    }
+
+    removePart(): void {
+        let values = this.state.values;
+
+        values.pop();
+
+        this.setState({
+            values: values
+        });
+
+        this.props.onChange(this.getRenderedString(values));
     }
 
     render() {
         return <Fragment>
-            <label>First half</label>
-            <ScriptHelper context={this.props.context} includeText={true} template={this.props.template} topLevel={false} onChange={(value) => { this.onChange(value, true) }}></ScriptHelper>
-            <label>Second half</label>
-            <ScriptHelper context={this.props.context} includeText={true} template={this.props.template} topLevel={false} onChange={(value) => { this.onChange(value, false) }}></ScriptHelper>
+            {this.state.values.map((_value, index) => {
+                return <Fragment key={index}>
+                    <label><strong>part {index + 1}</strong></label>
+                    <ScriptHelper context={this.props.context} includeText={true} template={this.props.template} topLevel={false} onChange={(value) => { this.onChange(value, index) }}></ScriptHelper>
+                </Fragment>
+            })}
+
+            <div className="input-group">
+                <button type="button" className="btn btn-primary" onClick={this.addPart}>Add</button>
+                {this.state.values.length > 2 && <button type="button" className="btn btn-danger" onClick={this.removePart}>Remove</button>}
+            </div>
         </Fragment>
     }
 }
