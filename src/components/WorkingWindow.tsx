51ff3f8499a4cb13c2de6b7d3a8e6681ae2fea74
Morten Turn Pedersen <motp@nodes.dk>
2019-12-03 15:57:27 +0100

    Made a simple modal

--- src/components/WorkingWindow.tsx
+++ src/components/WorkingWindow.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { Component, Fragment } from 'react'
+import { Component, Fragment } from 'react';
 import ArmTemplate from "../models/ArmTemplate";
 import ParameterForm from "./WorkingWindow/ParameterForm";
 import Parameter from "../models/Parameter";
@@ -24,9 +24,21 @@ interface WorkingWindowProps {
     editKey?: string;
 }
 
-export class WorkingWindow extends Component<WorkingWindowProps> {
+class WorkingWindowState {
+    showModal: boolean;
+}
+
+export class WorkingWindow extends Component<WorkingWindowProps, WorkingWindowState> {
     constructor(props: WorkingWindowProps) {
         super(props);
+
+        this.closeScriptHelper = this.closeScriptHelper.bind(this);
+        this.openScriptHelper = this.openScriptHelper.bind(this);
+
+        let state = new WorkingWindowState();
+        state.showModal = false;
+
+        this.state = state;
     }
 
     getHeadline(currentWindow: Windows, key?: string): string {
@@ -54,6 +66,18 @@ export class WorkingWindow extends Component<WorkingWindowProps> {
         return "Unknown window selected";
     }
 
+    closeScriptHelper(): void {
+        this.setState({
+            showModal: false
+        });
+    }
+
+    openScriptHelper(): void {
+        this.setState({
+            showModal: true
+        });
+    }
+
     render() {
         let headline: string = this.getHeadline(this.props.window, this.props.editKey);
 
@@ -70,11 +94,40 @@ export class WorkingWindow extends Component<WorkingWindowProps> {
             scriptContextType = ScriptContextType.Resources;
         }
 
+        const modalClass = this.state.showModal ? "modal show" : "modal";
+
         return (<Fragment>
-                {showScriptHelper && <ScriptHelper topLevel={true} template={this.props.template} context={scriptContextType}></ScriptHelper>}
-                <h2>{headline}</h2>
+                <div className="row">
+                    <div className="col">
+                        <h2>{headline}</h2>
+                    </div>
+                    <div className="col text-right">
+                        <button className="btn btn-primary" onClick={this.openScriptHelper}>Open script helper</button>
+                    </div>
+                </div>
                 {this.props.window === Windows.AddParameter && <ParameterForm parameter={null} onSubmit={this.props.onAddParameter}></ParameterForm> }
                 {this.props.window === Windows.EditParameter && <ParameterForm parameter={this.props.template.parameters[this.props.editKey]} name={this.props.editKey} onSubmit={this.props.onAddParameter}></ParameterForm>}
+                {showScriptHelper && <div className={modalClass} tabIndex={-1} role="dialog">
+                    <div className="modal-dialog" role="document">
+                        <div className="modal-content">
+                            <div className="modal-header">
+                                <h5 className="modal-title">Script helper</h5>
+                                <button type="button" className="close" onClick={this.closeScriptHelper} aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                                </button>
+                            </div>
+
+                            <div className="modal-body">
+                                <ScriptHelper topLevel={true} template={this.props.template} context={scriptContextType}></ScriptHelper>
+                            </div>
+
+                            <div className="modal-footer">
+                                <button className="btn btn-primary" type="button" onClick={this.closeScriptHelper}>Close</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            }
             </Fragment>)
     }
 }
