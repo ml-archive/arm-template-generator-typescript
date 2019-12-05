aae6ecf62d8b557d39d9b418f732fb3b80bcd51d
Morten Turn Pedersen <motp@nodes.dk>
2019-12-05 13:26:30 +0100

    Made resource input work

---
+++ src/components/Inputs/ResourceInput.tsx
@@ -0,0 +1,118 @@
+import { Component, Fragment, ChangeEvent } from "react";
+import React = require("react");
+import Select from "./Select";
+
+interface ResourceInputProps {
+    id: string;
+    value: string;
+    variables: string[];
+    parameters: string[];
+    onValueUpdated: (value: string) => void;
+    onNewParameterNameChanged: (name: string) => void;
+}
+
+class ResourceInputState {
+    value: string;
+    variableName: string;
+    type: ResourceInputOptions;
+}
+
+enum ResourceInputOptions {
+    Parameter,
+    Variable,
+    Custom
+}
+
+export class ResourceInput extends Component<ResourceInputProps, ResourceInputState> {
+    constructor(props: ResourceInputProps) {
+        super(props);
+
+        this.onTypeSelected = this.onTypeSelected.bind(this);
+        this.onValueSelected = this.onValueSelected.bind(this);
+        this.setVariableName = this.setVariableName.bind(this);
+
+        let state = new ResourceInputState();
+        state.type = ResourceInputOptions.Custom;
+
+        this.state = state;
+    }
+
+    onTypeSelected(option: string): void {
+        const key = option as keyof typeof ResourceInputOptions;
+
+        this.setState({
+            type: ResourceInputOptions[key],
+            value: "",
+            variableName: ""
+        });
+
+        this.props.onValueUpdated("");
+        this.props.onNewParameterNameChanged("");
+    }
+
+    onValueSelected(option: string): void {
+        const typeString = this.state.type === ResourceInputOptions.Parameter ? "parameter" : "variable";
+
+        this.setState({
+            value: option
+        });
+
+        this.props.onValueUpdated("[" + typeString + "('" + option + "')]");
+    }
+
+    setVariableName(event: ChangeEvent<HTMLInputElement>) {
+        this.setState({
+            variableName: event.currentTarget.value
+        });
+
+        this.props.onNewParameterNameChanged(event.currentTarget.value);
+    }
+
+    render() {
+        const options = [
+            ResourceInputOptions[ResourceInputOptions.Parameter],
+            ResourceInputOptions[ResourceInputOptions.Variable],
+            ResourceInputOptions[ResourceInputOptions.Custom]
+        ];
+
+        let typeValue = ResourceInputOptions[this.state.type];
+
+        let valueLabel: string;
+
+        switch(this.state.type) {
+            case ResourceInputOptions.Variable:
+                valueLabel = "Variable";
+                break;
+            case ResourceInputOptions.Parameter:
+                valueLabel = "Parameter";
+                break;
+            case ResourceInputOptions.Custom:
+            default:
+                valueLabel = "Value";
+                break;
+        }
+
+        return <Fragment>
+            <label htmlFor={this.props.id + "-type"}>Type</label>
+            <div className="input-group">
+                <Select id={this.props.id + "-type"} values={options} value={typeValue} hideEmpty={true} onOptionSelect={this.onTypeSelected}></Select>
+            </div>
+
+            <label htmlFor={this.props.id}>{valueLabel}</label>
+            <div className="input-group">
+                {this.state.type === ResourceInputOptions.Custom && this.props.children}
+                {this.state.type !== ResourceInputOptions.Custom && 
+                    <Select value={this.state.value} values={this.state.type === ResourceInputOptions.Parameter ? this.props.parameters : this.props.variables} required={true} id={this.props.id} onOptionSelect={this.onValueSelected}></Select> }
+            </div>
+
+            {this.state.type === ResourceInputOptions.Custom && <Fragment>
+                <label htmlFor={this.props.id + "-new-parameter-name"}>New parameter name ([a-Z][a-Z0-9]*). Leave empty if no parameter is to be made</label>
+                <div className="input-group">
+                    <input type="text" className="form-control" pattern="\w[\w\d]*" id={this.props.id + "-new-parameter-name"} value={this.state.variableName} onChange={this.setVariableName} />
+                </div>
+            </Fragment>}
+        </Fragment>
+    }
+}
+
+export default ResourceInput;
--- src/components/WorkingWindow/Resources/StorageAccountForm.tsx
+++ src/components/WorkingWindow/Resources/StorageAccountForm.tsx
@@ -3,9 +3,11 @@ import StorageAccount from "../../../models/Resources/StorageAccount";
 import { Fragment } from "react";
 import React = require("react");
 import Select from "../../Inputs/Select";
+import ResourceInput from "../../Inputs/ResourceInput";
 
 class StorageAccountFormState extends ResourceFormState {
     kind: string;
+    kindParameterName: string;
 }
 
 export class StorageAccountForm extends ResourceTypeForm<StorageAccount, StorageAccountFormState> {
@@ -16,8 +18,12 @@ export class StorageAccountForm extends ResourceTypeForm<StorageAccount, Storage
     constructor(props: ResourceFormProps<StorageAccount>) {
         super(props);
 
+        this.onKindParameterNameUpdated = this.onKindParameterNameUpdated.bind(this);
+        this.onKindSelected = this.onKindSelected.bind(this);
+
         let state = this.getBaseState(props);
         state.kind = "";
+        state.kindParameterName = "";
 
         if(props.resource) {
             if(props.resource.kind) {
@@ -26,6 +32,12 @@ export class StorageAccountForm extends ResourceTypeForm<StorageAccount, Storage
         }
 
         this.state = state;
+    }
+
+    onKindParameterNameUpdated(name: string) {
+        this.setState({
+            kindParameterName: name
+        });
     }
 
     onKindSelected(option: string) {
@@ -41,9 +53,9 @@ export class StorageAccountForm extends ResourceTypeForm<StorageAccount, Storage
     getSpecificMarkup(): JSX.Element {
         return (<Fragment>
             <label htmlFor="resource-kind">Kind</label>
-            <div className="input-group">
-                <Select hideEmpty={true} id="resource-kind" required={true} value={this.state.kind} values={StorageAccount.allowedKinds} onOptionSelect={this.onKindSelected}></Select>
-            </div>
+            <ResourceInput id="resource-kind" value={this.state.kind} parameters={Object.keys(this.props.template.parameters)} variables={Object.keys(this.props.template.variables)} onValueUpdated={this.onKindSelected} onNewParameterNameChanged={this.onKindParameterNameUpdated}>
+                <Select id="resource-kind" required={true} value={this.state.kind} values={StorageAccount.allowedKinds} onOptionSelect={this.onKindSelected}></Select>
+            </ResourceInput>
         </Fragment>);
     }
 }
